9ad1f04cf3d828db0a89c448e27b1f7b1e8f3aa9
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-03-20 19:04:30 +0100

    delete threads page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar"
 import FooterBar from "./components/FooterBar"
 import Home from "./components/Home"
 import ServicesPage from "./components/Services"
-import ThreadsPage from "./components/Threads"
+
 import ContactPage from "./components/Contact"
 import mainTheme from "./themeConfig"
 
@@ -18,7 +18,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/services" element={<ServicesPage />} />
-                    <Route path='/threads' element={<ThreadsPage />} />
+
                     <Route path='/contact' element={<ContactPage />} />
                 </Routes>
                 <FooterBar />
