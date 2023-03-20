34e231b217b658e795c5a5a7f34cdb5330651025
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-03-20 18:47:34 +0100

    delete Services page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from "./components/Navbar"
 import FooterBar from "./components/FooterBar"
 import Home from "./components/Home"
-import ServicesPage from "./components/Services"
 import ThreadsPage from "./components/Threads"
 import Contact from "./components/Contact"
 import mainTheme from "./themeConfig"
@@ -17,7 +16,7 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path='/services' element={<ServicesPage />} />
+
                     <Route path='/threads' element={<ThreadsPage />} />
                     <Route path='/contact' element={<Contact />} />
                 </Routes>
