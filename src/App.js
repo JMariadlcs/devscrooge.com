fd5bb4b41dcc557073db3d35bdbe15609a66e964
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-03-20 18:40:15 +0100

    delete Home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Box, ThemeProvider } from "@mui/material"
 import { Routes, Route } from 'react-router-dom'
 import Navbar from "./components/Navbar"
 import FooterBar from "./components/FooterBar"
-import Home from "./components/Home"
+
 import ServicesPage from "./components/Services"
 import ThreadsPage from "./components/Threads"
 import Contact from "./components/Contact"
@@ -16,7 +16,7 @@ function App() {
             <Box>
                 <Navbar />
                 <Routes>
-                    <Route path='/' element={<Home />} />
+
                     <Route path='/services' element={<ServicesPage />} />
                     <Route path='/threads' element={<ThreadsPage />} />
                     <Route path='/contact' element={<Contact />} />
