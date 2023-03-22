0a80a4ad55358f6eb9fbbb7ea79afff8f04ba4c4
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-03-22 18:43:05 +0100

    add new tweets and topics

--- src/components/Utils/Tweetsjson.js
+++ src/components/Utils/Tweetsjson.js
@@ -21,15 +21,82 @@ export const categories = [
                 "title": "Become blockchain dev roadmap",
                 "link": "https://twitter.com/devScrooge/status/1526905559215120384?s=20",
                 "language": "Spanish"
-            }
+            },
+            {
+                "title": "Learning Rust resources",
+                "link": "https://twitter.com/devScrooge/status/1587852029904703488?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "32h Patrick Collins course analysis (greetings to patrick 😜)",
+                "link": "https://twitter.com/devScrooge/status/1530222382748778497?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "How to create a decentralized Frontend",
+                "link": "https://twitter.com/devScrooge/status/1537423173901332480?s=20",
+                "language": "Spanish"
+            },
         ]
     },
     {
         "name": "BLOCKCHAIN SECURITY",
         "tweets": [
             {
-
+                "title": "Learning Smart Contract Audits resources",
+                "link": "https://twitter.com/devScrooge/status/1578713955043725312?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "nonReentrant vs globalNonReentrant modifier",
+                "link": "https://twitter.com/devScrooge/status/1633146824394436617?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "Reentrancy attack with ERC721 tokens",
+                "link": "https://twitter.com/devScrooge/status/1629445613891776514?s=20",
+                "language": "Spanish"
             }
+        ]
+    },
+    {
+        "name": "SM SECURITY CHALLENGE",
+        "tweets": [
+            {
+                "title": "Smart Contract Security challenge 1",
+                "link": "https://twitter.com/devScrooge/status/1623018807857127424?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "Smart Contract Security challenge 2",
+                "link": "https://twitter.com/devScrooge/status/1625193153857990658?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "Smart Contract Security challenge 3",
+                "link": "https://twitter.com/devScrooge/status/1628070645195059201?s=20",
+                "language": "Spanish"
+            },
+        ]
+    },
+    {
+        "name": "SOLIDITY TIPS",
+        "tweets": [
+            {
+                "title": "StackTooDeepException",
+                "link": "https://twitter.com/devScrooge/status/1623683199066066944?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "Selfdestruct function",
+                "link": "https://twitter.com/devScrooge/status/1624458722188705792?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "Gas saving",
+                "link": "https://twitter.com/devScrooge/status/1625520788269834240?s=20",
+                "language": "Spanish"
+            },
         ]
     },
     {
@@ -44,7 +111,32 @@ export const categories = [
                 "title": "Secure your private keys",
                 "link": "https://twitter.com/devScrooge/status/1533426256959651840?s=20",
                 "language": "Spanish"
-            }
+            },
+            {
+                "title": "Airdrops SCAM",
+                "link": "https://twitter.com/devScrooge/status/1550151160362844162?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "$BLUR airdrop tricky txs",
+                "link": "https://twitter.com/devScrooge/status/1625552174859137026?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "MetaMask new feature",
+                "link": "https://twitter.com/devScrooge/status/1622595998639091713?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "Solidity 0.8.18 release",
+                "link": "https://twitter.com/devScrooge/status/1621493772835254273?s=20",
+                "language": "Spanish"
+            },
+            {
+                "title": "Metamask token interface",
+                "link": "https://twitter.com/devScrooge/status/1577210605874081797?s=20",
+                "language": "Spanish"
+            },
         ]
     },
 ]
