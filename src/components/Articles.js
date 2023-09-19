a421265fb290631c22536c14767cb40c20ebb4fe
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-09-19 23:56:44 +0200

    update articles page

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,6 +1,4 @@
 import { Box, Typography, styled, Button } from "@mui/material"
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
 import { StyledBox } from "./Custom/CustomComponents";
 import { BannerMedium } from "./Utils/MediumBanner";
 import { ArticlesTittle } from "./Custom/CustomComponents";
@@ -8,8 +6,6 @@ import { useNavigate } from 'react-router-dom';
 
 const ArticlesPage = () => {
     const navigate = useNavigate();
-    const theme = useTheme();
-    const isXlScreen = useMediaQuery(theme.breakpoints.up('xl'));
 
     const ArticleBox = styled(Box)(({ theme }) => ({
         alignItems: 'center',
@@ -18,10 +14,11 @@ const ArticlesPage = () => {
         boxShadow: '0px 0px 20px 1px rgba(255, 255, 255, 0.2)',
         flexGrow: '0',
         flexDirection: 'column',
-        margin: '50px -20px 10px 0px', // Decreased margin here
-        maxWidth: '30%',
-        padding: isXlScreen ? '0 100px' : ' 0 50px',
-        width: '100px%',
+        margin: '10px',
+        height: '100%',
+        maxWidth: '25%',
+        padding: '0 10px 0 10px',
+        width: '100%',
         borderRadius: 10,
         [theme.breakpoints.down('lg')]: {
             flexDirection: 'column',
@@ -43,11 +40,11 @@ const ArticlesPage = () => {
     }
 
     return (
-        <Box className="home-box" sx={{ backgroundColor: 'primary.main' }} margin="-25px 0 0 0">
+        <Box className="home-box" sx={{ backgroundColor: 'primary.main' }} margin="-25px 0 0 0" padding='0 0 10px 0'>
             <ArticlesTittle />
             
             
-            <Box className="home-box" sx={{ backgroundColor: 'primary.main', height: '140vh' }} padding='0 0 150px 0' margin="-25px 0 0 0">
+            <Box className="home-box" sx={{ backgroundColor: 'primary.main', height: '100%' }} padding='0 0 150px 0' margin="-25px 0 0 0">
                 <StyledBox className="home-hire-box" margin="20px 0 100px 0" padding="0px 200px">
                 <ArticleBox className="home-buy-code4arena101-box">
                         <Typography variant="h5" component="h2" gutterBottom margin='30px 0 20px 0' align="center" alignSelf="center" fontWeight='bold' color='yellow'>
@@ -67,6 +64,32 @@ const ArticlesPage = () => {
                             <Box sx={{ ml: 2, margin: '0 0 10px 0' }} >
                                 <Typography color="textPrimary" variant="body1" component="p" gutterBottom margin="20px 0 10px 0">
                                     <span style={{ color: 'white' }}></span>  Access 'Deep Dives' and learn in deep about Slippage, Oracle Manipulation, Signature Malleability...
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </ArticleBox>
+
+                    <ArticleBox className="home-buy-code4arena101-box">
+                        <Typography variant="h5" component="h2" gutterBottom color="textSecondary" margin='30px 0 20px 0' align="center" alignSelf="center" fontWeight='bold'>
+                            Funding Rate and Perpetual Protocols
+                        </Typography>
+                        <BannerMedium src={"medium0"} />
+                        <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                            <Button variant="contained" href={"https://medium.com/@devscrooge/funding-rate-and-perpetual-protocols-d0da5ca7a5fb"} target="_blank" s sx={{
+                                borderRadius: 2,
+                                border: '1px solid rgba(27, 123, 236, 1)',
+                                boxShadow: '0px 0px 20px 1px rgba(255, 255, 255, 0.2)',
+                                margin: '0 0 10px 0'
+                            }}>
+                                Read
+                            </Button>
+
+
+                            <Box sx={{ ml: 2, margin: '0 0 10px 0' }} >
+                                <Typography color="textPrimary" variant="body1" component="p" gutterBottom margin="20px 0 10px 0">
+                                    <span style={{ color: 'white' }}></span> If you are into blockchain security space you may have notice that lots of Perpetual protocols are emerging (GMX, etc). Auditing this type of protocols is not easy as the introduce complex terms, for example...
+
+                                    Have you ever heard about the "Funding Rate"?
                                 </Typography>
                             </Box>
                         </Box>
